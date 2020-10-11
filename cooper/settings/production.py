2113b18e741a0ca1eec772233dbda4dc4c332d20
oluwatomisin James <jamesoluwatomisinomotosho@gmail.com>
2020-10-11 12:28:22 +0400

    first commit

---
+++ cooper/settings/production.py
@@ -0,0 +1,13 @@
+from .base import *
+import dj_database_url
+
+
+#Override defualt setings here
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
